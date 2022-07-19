e082c5a045151ca095637e58806aa4b738eb36c7
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-07-19 17:16:51 +0900

    Object

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const toBuy = ["potato", "tomato", "pizza"];
-toBuy.push("apple");
-// To-do list : Array.push()
-console.log(toBuy);
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+player.lastName = "potato";
+player.points += 15;
+console.log(player);
