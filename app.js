a8ef6e73fd6aa19ac4cc94a81919354019161ee3
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-07-19 20:14:35 +0900

    Return

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
+  plus: function (a, b) {
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   divid: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   multiple: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   power: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.add(2, 2);
-calculator.minus(2, 2);
-calculator.divid(2, 2);
-calculator.multiple(2, 2);
-calculator.power(2, 3);
+const plus = calculator.plus(2, 2);
+const minus = calculator.minus(2, 2);
+const divid = calculator.divid(2, 2);
+const multiple = calculator.multiple(2, 2);
+const power = calculator.power(2, 2);
+
+console.log(plus);
+console.log(minus);
+console.log(divid);
+console.log(multiple);
+console.log(power);
