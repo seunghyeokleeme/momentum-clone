bf135b2bd7d0ed293f92a02f0b452284a528a906
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-07-19 19:53:40 +0900

    Recap2

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonsName) {
-    console.log("hello", otherPersonsName);
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divid: function (a, b) {
+    console.log(a / b);
+  },
+  multiple: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-player.sayHello("Devinan");
-player.sayHello("nico");
+calculator.add(2, 2);
+calculator.minus(2, 2);
+calculator.divid(2, 2);
+calculator.multiple(2, 2);
+calculator.power(2, 3);
