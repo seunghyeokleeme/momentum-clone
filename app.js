5a9b05eef4fdca69fc136720ee5a2ced3d280daf
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-07-19 20:22:18 +0900

    recap

--- app.js
+++ app.js
@@ -16,14 +16,6 @@ const calculator = {
   },
 };
 
-const plus = calculator.plus(2, 2);
-const minus = calculator.minus(2, 2);
-const divid = calculator.divid(2, 2);
-const multiple = calculator.multiple(2, 2);
-const power = calculator.power(2, 2);
+const plus = calculator.plus(2, 3);
 
 console.log(plus);
-console.log(minus);
-console.log(divid);
-console.log(multiple);
-console.log(power);
