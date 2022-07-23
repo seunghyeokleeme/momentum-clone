eacdcbb04062356245b9403f7c053cac09d70b1f
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-07-23 16:34:03 +0900

    Form submission

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.dir(loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
