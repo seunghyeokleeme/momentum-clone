f9ae1419c07d9318571fe84d2d499ef314e165b0
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-01 02:04:01 +0900

    bgImg css

--- js/background.js
+++ js/background.js
@@ -4,4 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add("bgImage");
+
 document.body.appendChild(bgImage);
